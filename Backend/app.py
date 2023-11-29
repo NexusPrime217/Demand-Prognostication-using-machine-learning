ab19aeae17b8678372257d042fed934161c1120e
unknown <tulekarabhishek.2@gmail.com>
2023-11-29 16:07:38 +0530

    req txt added and app.py edited

--- Backend/app.py
+++ Backend/app.py
@@ -43,4 +43,4 @@ def result():
     return render_template("results.html", prediction = Y_pred)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9457)
+    app.run(host="0.0.0.0",port=5000)
